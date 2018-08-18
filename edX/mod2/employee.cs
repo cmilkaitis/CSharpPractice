4948c04bfee64a9d56090f3a00dcca63fc1861ae
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-08-18 11:11:22 -0400

    base employee class

---
+++ edX/mod2/employee.cs
@@ -0,0 +1,51 @@
+using System;
+namespace Mod2_Lab1
+{
+    public class Employee
+    {
+        // private variables
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+        private static int employeeCount = 1;
+
+        // public properties
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public int ID { get; set; }
+
+        // constructor
+        public Employee(string name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        // 'get' methods
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public int getID()
+        {
+            return this.ID;
+        }
+
+        public virtual String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        public virtual String employeeStatus()
+        {
+            return toString() + " is in the company's system";
+        }
+    }
+}
